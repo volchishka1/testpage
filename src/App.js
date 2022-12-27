dba885526960ec03f9b39f32b604077e99b75652
macwolf <romazverb18@gmsil.com>
2022-12-27 23:25:46 +0300

    add new component header

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
-import logoSvg from './assets/logoSvg.svg';
-import vectorDown from './assets/vectorDown.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import {Header} from "./components/header/header";
+
+export const App = () => {
   return (
     <div className="App">
-      <header className="App-header-container">
-          <div className="Header-logo-container">
-              <img src={logoSvg} alt="logoSvg" />
-              <span className="Logo-text">antools.</span>
-          </div>
-          <div className="Header-text-center-container">
-              <span>Home</span>
-              <div>
-                  <span>Categories</span>
-                  <img className="vector-down" src={vectorDown} alt={vectorDown}/>
-              </div>
-              <span>My collections</span>
-              <span>Blog</span>
-          </div>
-          <div className="button-container">
-              <span className="login-text">
-                  Login
-              </span>
-              <button className="sign-up-button">
-                  <span className="sing-up-text">
-                  Sign Up
-                  </span>
-              </button>
-          </div>
-      </header>
+      <Header />
     </div>
   );
 }
---
+++ src/components/header/header.js
@@ -0,0 +1,33 @@
+import './headerStyle.css'
+import logoSvg from "../../assets/logoSvg.svg";
+import vectorDown from "../../assets/vectorDown.svg";
+
+export const Header = () => {
+    return (
+        <header className="App-header-container">
+            <div className="Header-logo-container">
+                <img src={logoSvg} alt="logoSvg" />
+                <span className="Logo-text">antools.</span>
+            </div>
+            <div className="Header-text-center-container">
+                <span>Home</span>
+                <div>
+                    <span>Categories</span>
+                    <img className="vector-down" src={vectorDown} alt={vectorDown}/>
+                </div>
+                <span>My collections</span>
+                <span>Blog</span>
+            </div>
+            <div className="button-container">
+              <span className="login-text">
+                  Login
+              </span>
+                <button className="sign-up-button">
+                  <span className="sing-up-text">
+                  Sign Up
+                  </span>
+                </button>
+            </div>
+        </header>
+);
+}
