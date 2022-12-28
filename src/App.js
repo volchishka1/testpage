ff914990624a975dc7f8c6b6f43c146c564b2415
macwolf <romazverb18@gmsil.com>
2022-12-29 01:33:02 +0300

    refactor app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-
 import './App.css';
 import {Header} from "./components/header/header";
 import {DescriptionOne} from "./components/descriptionOne/descriptionOne";
+import {Tools} from "./components/tools/tools";
 
 export const App = () => {
   return (
     <div className="App">
       <Header />
       <DescriptionOne />
+      <Tools />
     </div>
   );
 }
